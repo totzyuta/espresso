9385d6a8960e18942dcbf7b23e7cccd58c5dd9be
totzYuta <yuta.totz@gmail.com>
2015-05-25 07:00:00 +0900

    Add nabe's program

---
+++ from_nabe/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>	/* free() を使う時に必要 */
+#include "define.h"
+
+TokenSt *nextToken(FILE *fp);
+// Node *node(FILE *fp);
+Node *Oparser(FILE *fp);
+
+
+/* 使い方表示関数のプロトタイプ宣言                */
+/* このファイルでしか使わないので staticにしている */
+static void usage(char *s);
+
+int main(int argc, char *argv[]){
+  FILE		*fp;
+  TokenSt	*token;
+  Node *node;
+
+  /*--< ファイル名を指定しないとエラー >--*/
+  if (argc != 2 ) usage(argv[0]);
+  
+  /*--< ファイルをオープンする >--*/
+  if ((fp = fopen(argv[1],"r")) == NULL){
+    fprintf(stderr,"No such file (%s)\n",argv[1]);
+    exit(1);
+  }
+
+  // Parsing operators and operands
+  node = Oparser(fp);  
+
+  fclose(fp);
+
+  printf("Program Ends! Bye :)");
+
+  return 0;
+}
+
+/*--< このプログラムの説明を表示 >--*/
+static void usage(char *s){
+  fprintf(stderr, "Usage: %s < filename >\n",s);
+  exit(1);
+}
