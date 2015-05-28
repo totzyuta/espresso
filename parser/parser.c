f22151a980ad2591de5e0605358f7301db9cec63
totzYuta <zfbfmtmxaz@gmail.com>
2015-05-28 22:52:30 +0900

    Write draft of functions of parser

---
+++ parser/parser.c
@@ -0,0 +1,75 @@
+// プログラム全体の解析
+void parse_program(FILE *fp) {
+}
+
+// 変数宣言部の解析
+void parse_define(FILE *fp) {
+}
+
+// 宣言文の解析
+void parse_define_statement(FILE *fp) {
+}
+
+// 配列宣言の解析
+void parse_define_array(FILE *fp) {
+}
+
+// 文集合の解析
+void parse_statements(FILE *fp) {
+}
+
+// 文の解析
+void parse_statement(FILE *fp) {
+}
+
+// 代入文の解析
+void parse_assign_value(FILE *fp) {
+}
+
+// 算術式の解析
+void parse_operation(FILE *fp) {
+}
+
+// 項の解析
+void parse_terms(FILE *fp) {
+}
+
+// 因子の解析
+void parse_factor(FILE *fp) {
+}
+
+// 変数の解析
+void parse_value(FILE *fp) {
+}
+
+// 引数の解析
+void parse_argument(FILE *fp) {
+}
+
+// 関数の解析
+void parse_func(FILE *fp) {
+}
+
+// 関数宣言文の解析
+void parse_define_func(FILE *fp) {
+}
+
+// ループ文の解析
+void parse_while(FILE *fp) {
+}
+
+// 条件分岐分の解析
+void parse_if(FILE *fp) {
+}
+
+// 条件式の解析
+void parse_compare(FILE *fp) {
+}
+
+// 比較演算子の解析
+void parse_relational(FILE *fp) {
+}
+
+// 識別子の解析
+void parse_identifier(FILE *fp) {
+}
